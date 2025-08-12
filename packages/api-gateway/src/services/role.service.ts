97ba3a083605839d6096222ea07fc51705b61ae6
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-08-12 08:07:22 +0530

    Adding roles types to role.service

--- packages/api-gateway/src/services/role.service.ts
+++ packages/api-gateway/src/services/role.service.ts
@@ -2,9 +2,10 @@ import { eq } from 'drizzle-orm';
 import { db } from '../db';
 import { roles } from '../db/schema/roles';
 import { logger } from '../utils/logger.utils';
+import { UserRole } from '@monorepo/shared';
 
 export class RoleService {
-  static async findRoleByName(roleName: string) {
+  static async findRoleByName(roleName: UserRole) {
     try {
       const role = await db.query.roles.findFirst({
         where: eq(roles.name, roleName),

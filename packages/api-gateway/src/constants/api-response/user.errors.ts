09aedd27ba688ec1a5b1fd6b0d9e42f80f6975c1
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-08-12 08:06:58 +0530

    Added messages

--- packages/api-gateway/src/constants/api-response/user.errors.ts
+++ packages/api-gateway/src/constants/api-response/user.errors.ts
@@ -1,3 +1,4 @@
 export const USER_ERRORS = {
   USER_NOT_FOUND: 'User not found',
+  EMAIL_ALREADY_EXISTS: 'Email already exists',
 };
--- packages/api-gateway/src/constants/api-response/validation.errors.ts
+++ packages/api-gateway/src/constants/api-response/validation.errors.ts
@@ -2,4 +2,5 @@ export const VALIDATION_ERRORS = {
   MISSING_FIELDS: 'Required fields are missing',
   INVALID_FORMAT: 'Request format is invalid',
   EMAIL_PASSWORD_REQUIRED: 'Email and Password are required',
+  INVALID_SIGNUP_DATA: 'Invalid signup data provided',
 };

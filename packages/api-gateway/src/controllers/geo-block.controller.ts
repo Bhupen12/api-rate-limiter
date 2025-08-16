06d2efc0ab65769ea41e0e6076d2d085ca069658
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-08-16 11:54:53 +0530

    Added Redis constants

--- packages/api-gateway/src/controllers/geo-block.controller.ts
+++ packages/api-gateway/src/controllers/geo-block.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { API_RESPONSES } from '../constants';
 import { failure, success } from '../utils/response.utils';
 import { isIP } from 'net';
+import { REDIS_GEO_BLOCK_KEY } from '../constants/redis.constants';
 
 export class GeoBlockController {
   static async addToIpWhiteList(req: Request, res: Response) {
@@ -9,7 +10,7 @@ export class GeoBlockController {
     if (!ip || !isIP(ip)) {
       return failure(res, 400, API_RESPONSES.GEO_BLOCK_ERRORS.INVALID_IP);
     }
-    await req.redis.sadd('geo:whitelist:ips', ip);
+    await req.redis.sadd(REDIS_GEO_BLOCK_KEY.ipWhitelist, ip);
     const response = { ip: ip };
     return success(res, response, 'IP added to whitelist successfully');
   }
@@ -19,7 +20,7 @@ export class GeoBlockController {
     if (!ip || !isIP(ip)) {
       return failure(res, 400, API_RESPONSES.GEO_BLOCK_ERRORS.INVALID_IP);
     }
-    await req.redis.sadd('geo:blocklist:ips', ip);
+    await req.redis.sadd(REDIS_GEO_BLOCK_KEY.ipBlacklist, ip);
     const response = { ip: ip };
     return success(res, response, 'IP added to blacklist successfully');
   }
@@ -29,28 +30,28 @@ export class GeoBlockController {
     if (!country || typeof country !== 'string') {
       return failure(res, 400, API_RESPONSES.GEO_BLOCK_ERRORS.INVALID_COUNTRY);
     }
-    await req.redis.sadd('geo:blocklist:countries', country);
+    await req.redis.sadd(REDIS_GEO_BLOCK_KEY.countryBlocklist, country);
     const response = { country: country };
     return success(res, response, 'Country added to blocklist successfully');
   }
 
   static async removeFromIpWhiteList(req: Request, res: Response) {
     const { ip } = req.params;
-    await req.redis.srem('geo:whitelist:ips', ip);
+    await req.redis.srem(REDIS_GEO_BLOCK_KEY.ipWhitelist, ip);
     const response = { ip: ip };
     return success(res, response, 'IP removed from whitelist successfully');
   }
 
   static async removeFromIpBlacklist(req: Request, res: Response) {
     const { ip } = req.params;
-    await req.redis.srem('geo:blocklist:ips', ip);
+    await req.redis.srem(REDIS_GEO_BLOCK_KEY.ipBlacklist, ip);
     const response = { ip: ip };
     return success(res, response, 'IP removed from blacklist successfully');
   }
 
   static async removeFromCountryBlocklist(req: Request, res: Response) {
     const { countryCode } = req.params;
-    await req.redis.srem('geo:blocklist:countries', countryCode);
+    await req.redis.srem(REDIS_GEO_BLOCK_KEY.countryBlocklist, countryCode);
     const response = { country: countryCode };
     return success(
       res,
--- packages/api-gateway/src/middleware/geo-block.middleware.ts
+++ packages/api-gateway/src/middleware/geo-block.middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import geoip from 'geoip-lite';
 import Redis from 'ioredis';
-import { API_RESPONSES } from 'src/constants';
-import { logger } from 'src/utils/logger.utils';
-import { failure } from 'src/utils/response.utils';
+import { API_RESPONSES } from '../constants';
+import { logger } from '../utils/logger.utils';
+import { failure } from '../utils/response.utils';
 import { getClientIp } from '../utils/get-client-ip';
+import { REDIS_GEO_BLOCK_KEY } from '../constants/redis.constants';
 
 export const geoBlockMiddleware = async (
   req: Request,
@@ -19,13 +20,19 @@ export const geoBlockMiddleware = async (
 
   try {
     // check whitelist
-    const isWhiteListed = await redis.sismember('geo:whitelist:ips', clientIp);
+    const isWhiteListed = await redis.sismember(
+      REDIS_GEO_BLOCK_KEY.ipWhitelist,
+      clientIp
+    );
     if (isWhiteListed) {
       return next();
     }
 
     // check blacklist
-    const isBlackListed = await redis.sismember('geo:blocklist:ips', clientIp);
+    const isBlackListed = await redis.sismember(
+      REDIS_GEO_BLOCK_KEY.ipBlacklist,
+      clientIp
+    );
     if (isBlackListed) {
       logger.warn(`Blocked request from blacklisted IP: ${clientIp}`);
       return failure(res, 403, API_RESPONSES.GEO_BLOCK_ERRORS.BLOCKED);
@@ -35,7 +42,7 @@ export const geoBlockMiddleware = async (
     const geo = geoip.lookup(clientIp);
     if (geo && geo.country) {
       const isCountryBlocked = await redis.sismember(
-        'geo:blocklist:countries',
+        REDIS_GEO_BLOCK_KEY.countryBlocklist,
         geo.country
       );
       if (isCountryBlocked) {

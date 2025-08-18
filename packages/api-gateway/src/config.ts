72b667bbb4853304806f94416c37c7fd270349b9
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-08-18 08:38:52 +0530

    Added more config

--- packages/api-gateway/src/config.ts
+++ packages/api-gateway/src/config.ts
@@ -21,16 +21,30 @@ export const config = {
     jwt: process.env.JWT_SECRET!,
   },
   ipqualityscore: {
-    apiKey: process.env.ABUSE_IP_DB_API_KEY!,
+    apiKey: process.env.IPQS_API_KEY!,
     baseUrl:
-      process.env.ABUSE_IP_DB_URL || 'https://api.abuseipdb.com/api/v2/check',
-    maxAgeInDays: parseInt(process.env.ABUSE_IP_DB_MAX_AGE || '50', 10),
+      process.env.IPQS_API_URL || 'https://ipqualityscore.com/api/json/ip',
+    maxAgeInDays: parseInt(process.env.IPQS_MAX_AGE || '50', 10),
     reputationCacheTtl: parseInt(
-      process.env.REPUTATION_CACHE_TTL || '3600',
+      process.env.IPQS_REPUTATION_CACHE_TTL || '3600',
       10
     ),
     reputationBlockThreshold: parseInt(
-      process.env.REPUTATION_BLOCK_THRESHOLD || '50',
+      process.env.IPQS_REPUTATION_BLOCK_THRESHOLD || '50',
+      10
+    ),
+  },
+  abuseipdb: {
+    apiKey: process.env.ABUSE_IP_DB_API_KEY!,
+    baseUrl:
+      process.env.ABUSE_IP_DB_URL || 'https://api.abuseipdb.com/api/v2/check',
+    maxAgeInDays: parseInt(process.env.ABUSE_IP_DB_MAX_AGE || '90', 10),
+    reputationCacheTtl: parseInt(
+      process.env.ABUSE_IP_DB_REPUTATION_CACHE_TTL || '3600',
+      10
+    ),
+    reputationBlockThreshold: parseInt(
+      process.env.ABUSE_IP_DB_REPUTATION_BLOCK_THRESHOLD || '90',
       10
     ),
   },
